b0f80362a17d41812a4ce68e8aaf9abc9a6d0074
JonWLS <75321066+jonathanWongLS@users.noreply.github.com>
2023-06-25 20:36:29 +0800

    Fixed create LinkedList method where it did not include the last element of each list's data

--- Blind75/LinkedList/LinkedList.java
+++ Blind75/LinkedList/LinkedList.java
@@ -2,7 +2,7 @@ package Blind75.LinkedList;
 
 public class LinkedList {
 
-    private ListNode head;
+    private ListNode head = null;
 
     public LinkedList() {}
 
@@ -13,25 +13,37 @@ public class LinkedList {
     public ListNode createLinkedList(int[] nodePayload) {        
         
         ListNode currentNode = null;
-        
-        for (int i = 0; i < nodePayload.length; i++) {
-         
-            if (i == 0) {
-                currentNode = new ListNode(nodePayload[i]); 
-                this.head = currentNode;
-                currentNode.next = new ListNode(nodePayload[++i]);
-            }
-            else {
-                if (i != nodePayload.length-1) { 
-                    currentNode.next = new ListNode(nodePayload[i]);
-                }
-                else {
-                    currentNode.next = null;
-                }
-            }
+        int i = 0;
 
+        if (nodePayload.length == 0) {
+            System.out.println("Node 0: null\n");
+            return this.head;
+        }
+
+        if (nodePayload.length == 1) {
+            this.head = new ListNode(nodePayload[0]);
+            System.out.println("Node 0: " + nodePayload[0]);
+            System.out.println("Node 1: null\n");
+            return this.head;
+        }
+        
+        for (i = 0; i < nodePayload.length; i++) {
+            if (i == 0) {
+                currentNode = new ListNode(nodePayload[i]);
+                this.head = currentNode;
+                System.out.println("Node " + Integer.toString(i) + ": " + nodePayload[i]);
+                currentNode.next = new ListNode(nodePayload[++i]);
+                System.out.println("Node " + Integer.toString(i) + ": " + nodePayload[i]);
+            }     
+            else {
+                System.out.println("Node " + Integer.toString(i) + ": " + nodePayload[i]);
+                currentNode.next = new ListNode(nodePayload[i]);
+            }
+    
             currentNode = currentNode.next;
         }
+
+        System.out.println("Node " + Integer.toString(i) + ": null\n");
 
         return this.head;
     }
